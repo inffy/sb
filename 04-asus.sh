38b8a19a022b928afccf3807ef224febaa451631
inffy <juha@uotila.org>
2022-10-23 16:16:08 +0300

    Update 04-asus.sh
    Added the needed mods and enablements

--- 04-asus.sh
+++ 04-asus.sh
@@ -9,6 +9,27 @@ sudo systemctl enable --now supergfxd
 cd ~/Projects
 
 # clone the asus misc stuff repo
-git clone https://gitlab.com/marcaux/asus-g14-2022
+git clone https://gitlab.com/inffy/g14-2022
+
+cd g14-2022
+
+echo "Installing scripts.."
+sudo ./install
+
+# Move the asusd profile.conf to right place
+echo "Moving asusd fan curve profile to the correct place"
+sudo cp etc/asusd/profile.conf /etc/asusd/
+
+echo "Restart asusd"
+sudo systemctl restart asusd
+
+echo "Enable the new Balanced profiles fan curves"
+asusctl fan-curve -m balanced -e true
+
+echo "Enable the new quiet profile fan curves"
+asusctl fan-curve -m quiet -e true
+
+echo "And finally the performance profile"
+asusctl fan-curve -m performance -e true
 
 echo "Repos cloned, reboot first :)"
