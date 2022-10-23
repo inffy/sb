9227490f53891313e18a83c908ded03fe4e05419
inffy <juha@uotila.org>
2022-10-23 16:28:22 +0300

    Explicitly install gstreamer and ffmpeg 22.08

--- 01-desktop.sh
+++ 01-desktop.sh
@@ -4,16 +4,13 @@ set -eux
 # Don't run this script as root
 [ ! "$UID" -eq 0 ] || { echo "This script must not be run as root."; exit 1;}
 
+gnome-extensions disable background-logo@fedorahosted.org
 dconf write /org/gnome/shell/disable-extension-version-validation "true" #yolo
 dconf write /org/gnome/shell/disable-user-extensions "false"
 dconf write /org/gnome/desktop/privacy/send-software-usage-stats "false"
 dconf write /org/gnome/software/allow-updates "false"
 dconf write /org/gnome/software/download-updates "false"
 dconf write /org/gnome/software/download-updates-notify "false"
-
-
-
-gnome-extensions disable background-logo@fedorahosted.org
 
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Settings.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Evolution.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop', 'org.gnome.TextEditor.desktop', 'com.onepassword.OnePassword.desktop', 'com.discordapp.Discord.desktop', 'org.telegram.desktop.desktop', 'com.irccloud.desktop.desktop', 'com.github.eneshecan.WhatsAppForLinux.desktop', 'im.riot.Riot.desktop', 'com.slack.Slack.desktop', 'org.filezillaproject.Filezilla.desktop', 'org.onlyoffice.desktopeditors.desktop', 'sh.cider.Cider.desktop', 'io.github.shiftey.Desktop.desktop', 'com.google.Chrome.desktop', 'com.microsoft.Edge.desktop', 'org.gimp.GIMP.desktop']"
 
