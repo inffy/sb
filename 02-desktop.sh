e9edfd91a744b283de5ec64abd1dd234204ace4d
inffy <juha@uotila.org>
2022-10-29 08:31:43 +0300

    thunderbird to favs

--- 02-desktop.sh
+++ 02-desktop.sh
@@ -12,7 +12,7 @@ dconf write /org/gnome/software/allow-updates "false"
 dconf write /org/gnome/software/download-updates "false"
 dconf write /org/gnome/software/download-updates-notify "false"
 
-dconf write /org/gnome/shell/favorite-apps "['org.gnome.Settings.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Evolution.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop', 'org.gnome.TextEditor.desktop', 'com.onepassword.OnePassword.desktop', 'com.discordapp.Discord.desktop', 'org.telegram.desktop.desktop', 'com.irccloud.desktop.desktop', 'com.github.eneshecan.WhatsAppForLinux.desktop', 'im.riot.Riot.desktop', 'com.slack.Slack.desktop', 'org.filezillaproject.Filezilla.desktop', 'org.onlyoffice.desktopeditors.desktop', 'sh.cider.Cider.desktop', 'io.github.shiftey.Desktop.desktop', 'com.google.Chrome.desktop', 'com.microsoft.Edge.desktop', 'org.gimp.GIMP.desktop']"
+dconf write /org/gnome/shell/favorite-apps "['org.gnome.Settings.desktop', 'org.gnome.Terminal.desktop', 'org.mozilla.Thunderbird', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop', 'org.gnome.TextEditor.desktop', 'com.onepassword.OnePassword.desktop', 'com.discordapp.Discord.desktop', 'org.telegram.desktop.desktop', 'com.irccloud.desktop.desktop', 'com.github.eneshecan.WhatsAppForLinux.desktop', 'im.riot.Riot.desktop', 'com.slack.Slack.desktop', 'org.filezillaproject.Filezilla.desktop', 'org.onlyoffice.desktopeditors.desktop', 'sh.cider.Cider.desktop', 'io.github.shiftey.Desktop.desktop', 'com.google.Chrome.desktop', 'com.microsoft.Edge.desktop', 'org.gimp.GIMP.desktop']"
 
 echo "Change to Projects dir"
 cd ~/Projects
