61e5538a15fcfe47c2e8d37c64bb85e0f7e220ab
inffy <juha@uotila.org>
2022-11-14 09:06:15 +0200

    add the needed nice fonts (meslo and ubuntu nerd font)

--- 02-desktop.sh
+++ 02-desktop.sh
@@ -48,5 +48,23 @@ cp .p10k.zsh ~/
 echo "change shell to zsh"
 sudo usermod -s /bin/zsh juha
 
+echo "Install needed fonts"
+# Fonts
+#
+# NerdFonts are basically fonts with a more rich set of emojis embeded
+mkdir -p ~/.local/share/fonts
+curl -sfLo "$HOME/.local/share/fonts/Ubuntu Mono Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
+curl -sfLo "$HOME/.local/share/fonts/Ubuntu Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Ubuntu/Regular/complete/Ubuntu%20Nerd%20Font%20Complete.ttf
+cp -R files/fonts/ubuntu-font-family/*.ttf $HOME/.local/share/fonts/
+cp -R files/fonts/meslo/*.ttf $HOME/.local/share/fonts/
+# Flush font cache
+fc-cache
+
+# Get current profile for terminal, and update the font
+current_profile=$(dconf list /org/gnome/terminal/legacy/profiles:/ | head -n1)
+# TODO(mc): We /could/ prompt the user if they want to keep the current font, use Ubuntu Mono, or Ubuntu Mono Nerd Font?
+dconf write /org/gnome/terminal/legacy/profiles:/${current_profile}use-system-font false
+dconf write /org/gnome/terminal/legacy/profiles:/${current_profile}font "'MesloLGS NF 10'"
+
 
 
