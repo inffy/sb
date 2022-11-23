a8275f8a25cb418ee25abee2cd88c9ca0d288c19
inffy <juha@uotila.org>
2022-11-23 14:44:43 +0200

    Update 00-first-clean.sh

--- 00-first-clean.sh
+++ 00-first-clean.sh
@@ -7,12 +7,15 @@ BITS=./bits
 source $BITS/common
 
 # Enable flathub
+echo "Adding Flathub repo"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Reinstall all the flatpaks from the fedora repo, so that they get installed from flathub
+echo "Reinstalling all the default flatpaks from flathub"
 flatpak install --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
 
 # Remove the useless fedora flatpak repo
+"Removing fedora repo and all the flatpaks installed from there"
 flatpak remote-delete fedora
 
 echo "Repo removed"
